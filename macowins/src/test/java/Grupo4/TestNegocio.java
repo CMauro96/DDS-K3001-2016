af94f3a82b213743a03edd1e5ae8f14c11d1325a
hernang94 <hernangallo@outlook.com>
2016-03-30 16:11:41 -0300

    Modificacion en el Test
    Hice que el test use el metodo de calcular fecha para que siempre tome
    la actual y funcione.

--- macowins/src/test/java/Grupo4/TestNegocio.java
+++ macowins/src/test/java/Grupo4/TestNegocio.java
@@ -2,6 +2,9 @@ package Grupo4;
 import Grupo4.Negocio;
 import Grupo4.Prenda;
 import Grupo4.Venta;
+
+import java.util.Calendar;
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -28,14 +31,22 @@ public void init(){
 	}
 @Test
 	public void pruebaVenta(){
-	Assert.assertEquals("23/3/2016", unaVenta.getFecha());
+	Assert.assertEquals(calcularFecha(), unaVenta.getFecha());
 	Assert.assertEquals(520, unaVenta.getPrecioFinal(),0);
 }
 @Test
 	public void pruebaGanancia(){
 	macowins.realizarVenta(saco, 4);
 	macowins.realizarVenta(saco, 10);
-	Assert.assertEquals(7280, macowins.gananciasDelDia("23/3/2016"), 0);
+	Assert.assertEquals(7280, macowins.gananciasDelDia(calcularFecha()), 0);
+}
+private String calcularFecha(){
+	Calendar c1 = Calendar.getInstance();
+	String dia = Integer.toString(c1.get(Calendar.DATE));
+	String mes = Integer.toString((c1.get(Calendar.MONTH)+1));
+	String anio = Integer.toString(c1.get(Calendar.YEAR));
+	return dia+"/"+mes+"/"+anio;
+	
 }
 	
 	
